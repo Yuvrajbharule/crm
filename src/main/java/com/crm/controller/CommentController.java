7a6c22191bbaceee327eb17d866e8d9b3776f995
Yuvrajbharule <113690977+Yuvrajbharule@users.noreply.github.com>
2024-11-27 10:49:50 +0530

    Update CommentController.java

--- src/main/java/com/crm/controller/CommentController.java
+++ src/main/java/com/crm/controller/CommentController.java
@@ -19,6 +19,8 @@ public class CommentController {
         System.out.println("10000");
         System.out.println("20000");
         System.out.println("20000");
+        System.out.println("20000");
+        System.out.println("20000");
 
     }
 
